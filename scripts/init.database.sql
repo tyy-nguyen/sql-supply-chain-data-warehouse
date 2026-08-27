218eb00c2777b2b8050813d7e392e55cb26f7561
Tyler Nguyen <tylernguyen2548@gmail.com>
2026-08-27 14:34:04 -0400

    Create SupplyChainWarehouse database and schemas
    This script initializes the SupplyChainWarehouse database and creates three schemas: bronze, silver, and gold.

---
+++ scripts/init.database.sql
@@ -0,0 +1,26 @@
+-- Use master to create a new database
+USE master;
+
+GO
+
+CREATE DATABASE SupplyChainWarehouse
+
+GO
+
+USE SupplyChainWarehouse
+
+GO
+
+-- Creates Schemas (Bronze/Silver/Gold)
+  
+CREATE SCHEMA bronze
+
+GO
+
+CREATE SCHEMA silver
+
+GO
+
+CREATE SCHEMA gold
+
+GO
